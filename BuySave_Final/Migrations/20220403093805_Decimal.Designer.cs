64913b4563d827584a70db39c00200bcbc2e6c92
Sujal Chand <sujalchand2526@gmail.com>
2022-04-03 21:39:59 +1200

    Added rating validation and changed Price variable from int to decimal

--- BuySave_Final/Migrations/20220403093805_Decimal.Designer.cs
+++ BuySave_Final/Migrations/20220403093805_Decimal.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BuySave_Final.Migrations
 {
     [DbContext(typeof(BuySave_FinalContextdb))]
-    partial class BuySave_FinalContextdbModelSnapshot : ModelSnapshot
+    [Migration("20220403093805_Decimal")]
+    partial class Decimal
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- BuySave_Final/Models/Review.cs
+++ BuySave_Final/Models/Review.cs
@@ -18,15 +18,18 @@ namespace BuySave_Final.Models
         public string ReviewText { get; set; }
         //An interger for product rating
         [Display(Name = "Product Rating ?/5")]
+        [Range(0, 5, ErrorMessage = "Rating must be between 0 and 5")]
         public int ItemRating { get; set; }
         //This string is the URL for the website the user purchased from
         [Display(Name = "Website link")]
         public string WebsiteURL { get; set; }
         //An interger for website rating
         [Display(Name = "Website Rating ?/5")]
+        [Range (0,5,ErrorMessage ="Rating must be between 0 and 5")]
         public int WebsiteRating { get; set; }
         [Display(Name = "Price NZD$")]
-        public int Price { get; set; }
+        [Range(0, 10000, ErrorMessage = "Price values are between 0 and 10000")]
+        public decimal Price { get; set; }
         //The relationships between the user and product classes
         public User User { get; set; }
         public Product Product { get; set; }
